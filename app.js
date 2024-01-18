5a5d09686d2ad586a49483ddac8f6e644529e1c6
javeria-farooq <gettingeducation6@gmail.com>
2024-01-18 18:12:09 +0500

    12 JS Assignment

---
+++ app.js
@@ -0,0 +1,39 @@
+// let arr = ['jav', 'far', 20]
+// document.write(arr[0 + 2])
+// document.write(arr[0], arr[2])
+// document.write(arr[0] + arr[2])
+
+// let i;
+
+// for(let i = 1; i<=10; i++) {
+//     document.write('hello world <br>')
+//     document.write(i + ') hello world <br>')
+    // check this k i++ ki wjah se iski value console pe kia hwi h:
+    // console.log(i)
+// }
+
+    // console.log(i)
+
+
+// for(i = 10; i>=0; i--) {
+//     document.write(i + '<br>')
+// }
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// for(i = 0; i<arr.length; i++) {
+//     document.write(arr[i] + '<br>')
+// }
+
+// let arr = ['a ','b ', 'c ', 'd ', 'e ', 'f ', 'g ', 'h ', 'i ']
+// for(let i = 0; i<arr.length; i++) {
+//     document.write(arr[i])
+// }
+
+//      task:
+
+// let tab = +prompt("Enter value for table")
+// let tabEnd = +prompt("Enter last value for table")
+
+// for(i = 1; i<=tabEnd; i++) {
+//     document.write(tab + ' x ' + i + ' = ' + tab*i + '<br>')
+// }
